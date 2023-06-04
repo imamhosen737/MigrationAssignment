83625ae5780a45cbf828e9851dcdd994feee9842
imamhosen737 <imamhosen737@gmail.com>
2023-06-04 23:44:23 +0600

    first commit

---
+++ database/migrations/2023_06_04_170351_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('orders')) {
+            Schema::create('orders', function (Blueprint $table) {
+                $table->id();
+                $table->bigInteger("product_id")->unsigned();
+                $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
+                $table->integer('quantity');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
